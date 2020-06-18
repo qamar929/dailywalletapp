22a6e6d465ddc9a98c254153963a9edcf1815695
qamar929 <52096570+qamar929@users.noreply.github.com>
2020-06-18 19:56:12 +0500

    final front end

---
+++ src/main/resources/static/precache-manifest.d50b2b45d4a60791a05fb057c2c6bdbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d232bf2a938e6e7514114961a32469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ddaffa45e3ed2e8d8b1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d68d23f18800098c78c6",
+    "url": "/static/css/main.1198c917.chunk.css"
+  },
+  {
+    "revision": "8ddaffa45e3ed2e8d8b1",
+    "url": "/static/js/2.cb607041.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cb607041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68d23f18800098c78c6",
+    "url": "/static/js/main.6cd97c42.chunk.js"
+  },
+  {
+    "revision": "1d039e839164f6abccc7",
+    "url": "/static/js/runtime-main.13b739d4.js"
+  },
+  {
+    "revision": "71fd085617ae37366dba7adaeaa431b7",
+    "url": "/static/media/wallpaper.71fd0856.jpg"
+  }
+]);
